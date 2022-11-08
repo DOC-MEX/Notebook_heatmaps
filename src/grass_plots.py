26bce2f54f6a51232fcc02f546e115f9327b6946
Daniel <marmandu@gmail.com>
2022-11-08 11:12:04 +0000

    Updating notes & code

--- src/grass_plots.py
+++ src/grass_plots.py
@@ -1,56 +1,75 @@
+import requests
 import json
 
 import numpy as np
 from functools import reduce
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from io import BytesIO
-import base64
-
-import os
-
 import plotly.express as px
+#from plotly.offline import plot as plotlyOffline
 
-def dict_phenotypes(pheno, plots):
-    """Extract traits of phenotypes 
+server_url = "http://localhost:2000/grassroots/public_backend"
 
-    Args:
-        pheno: list of phenotypes of a particular study 
-        plots     : plots data of a particular study
+'''
+Get study using id
+returns JSON from backend
+'''
+def get_plot(id):
+    plot_request = {
+            "services": [{
+                "so:name": "Search Field Trials",
+                "start_service": True,
+                "parameter_set": {
+                    "level": "advanced",
+                    "parameters": [{
+                        "param": "ST Id",
+                        "current_value": id
+                    }, {
+                        "param": "Get all Plots for Study",
+                        "current_value": True
+                    }, {
+                        "param": "ST Search Studies",
+                        "current_value": True
+                    }]
+                }
+            }]
+        }
+    res = requests.post(server_url, data=json.dumps(plot_request))
+    return json.dumps(res.json())
 
-    Returns:
-        dictionary: keys: phenotypes names, values: traits
-    """
+####################################################################
+def get_all_fieldtrials():
+    list_all_ft_request = {
+        "services": [
+            {
+                "so:name": "Search Field Trials",
+                "start_service": True,
+                "parameter_set": {
+                    "level": "simple",
+                    "parameters": [
+                        {
+                            "param": "FT Keyword Search",
+                            "current_value": ""
+                        },
 
-    names = []
-    traits = []
-    for key in pheno:
-        #print("-->", key)
-         
-
-        names.append(key)
-        traits.append(pheno[key]['definition']['trait']['so:name'])
-
-    phenoDict = dict(zip(names, traits))
-
-    for j in range(len(plots)):
-        if ( 'discard' in plots[j]['rows'][0] ):
-            pass
-        if ('observations' in plots[j]['rows'][0]):
-            for k in range(len(plots[j]['rows'][0]['observations'])):
-                if ('raw_value' in plots[j]['rows'][0]['observations'][k]):
-                    rawValue = plots[j]['rows'][0]['observations'][k]['raw_value']
-                if ('corrected_value' in plots[j]['rows'][0]['observations'][k]):
-                    rawValue = plots[j]['rows'][0]['observations'][k]['corrected_value']
-                if ( type(rawValue) == str):
-                    name = plots[j]['rows'][0]['observations'][k]['phenotype']['variable']
-                    if( name in phenoDict.keys() ):
-                        #print("Remove pheonotypes with non-numeric value:", phenoDict[name])
-                        del phenoDict[name]
-    
-    return phenoDict    
-
+                        {
+                            "param": "FT Study Facet",
+                            "current_value": True
+                        },
+                        {
+                            "param": "FT Results Page Number",
+                            "current_value": 0
+                        },
+                        {
+                            "param": "FT Results Page Size",
+                            "current_value": 500
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+    res = requests.post(server_url, data=json.dumps(list_all_ft_request))
+    return json.dumps(res.json())
 
 ###################################################################
 def lookup_keys(dictionary, keys, default=None):
@@ -69,7 +88,6 @@ def searchPhenotypeUnit(listPheno, value):
     name = listPheno[value]['definition']['unit']['so:name']
 
     return name
-
 
 ###################################################################
 def search_phenotype(list_observations, value):
@@ -137,9 +155,8 @@ def dict_phenotypes(pheno, plots):
     return phenoDict    
 
 
-
 ####################################################################
-def create_matrices(json, pheno, current_name, total_rows, total_columns):
+def numpy_data(json, pheno, current_name, total_rows, total_columns):
     """create numpy matrices for plotting
 
     Args:
@@ -269,13 +286,163 @@ def create_matrices(json, pheno, current_name, total_rows, total_columns):
     matrices.append(row)
     matrices.append(column)
     matrices.append(row_raw)
-    #matrices.append(row_acc)
+    matrices.append(row_acc)
     matrices.append(traitName)
     matrices.append(unit)
-    matrices.append(row_acc)
     matrices.append(plotsIds)
     
     return matrices
+
+
+########################################################################################
+'''
+create treatments array for plotly text 
+'''
+def treatments(arraysJson, rows, columns):
+
+    dt= np.dtype(('U', 80))
+    matrix = np.zeros((rows,columns), dtype=dt)
+    matrix[:] = "N/A"
+
+    for r in range(len(arraysJson)):
+        if  ( 'discard' in arraysJson[r]['rows'][0] ):
+            i = int( arraysJson[r]['row_index']    )
+            j = int( arraysJson[r]['column_index'] )
+            i=i-1
+            j=j-1
+            matrix[i][j] = 'N/A'
+        elif  ( 'blank' in arraysJson[r]['rows'][0] ):
+            i = int( arraysJson[r]['row_index']    )
+            j = int( arraysJson[r]['column_index'] )
+            i=i-1
+            j=j-1
+            matrix[i][j] = 'N/A'
+    
+
+        elif ( 'treatments' in arraysJson[r]['rows'][0] ):
+            i = int( arraysJson[r]['row_index']    )
+            j = int( arraysJson[r]['column_index'] )
+            i=i-1
+            j=j-1
+            value = []
+            label = []
+            treat = []
+            for k in range(len(arraysJson[r]['rows'][0]['treatments'])):
+                    value  = np.append(value, arraysJson[r]['rows'][0]['treatments'][k]["so:sameAs"] )
+                    label  = np.append(label, arraysJson[r]['rows'][0]['treatments'][k]["label"] )
+
+            for m in range(len(value)):
+                v1 = value[m]
+                v2 = label[m]
+                t  = v1 +' (' + v2 +')'        # combine name and label
+                treat  = np.append(treat, t)  # to create single matrix that contains all the treatment(s) info.  
+        
+            #string = ', '.join(value)
+            string = ', '.join(treat)
+            matrix[i][j] = string
+
+        ##else:
+        ##    matrix[i][j] = np.inf.  Possible Warning? No treatment saved in plots...
+
+    matrix  = matrix.flatten()
+    return matrix
+##############################################################################################
+'''
+test rendering plotly interactive heatmap
+'''
+def plotly_plot(numpy_matrix, accession, title, unit, IDs, treatments):
+
+    ##numpy_matrix = np.flipud(numpy_matrix)      # To Match order shown originally in JS code
+    #plotID      = np.flipud(IDs)        
+    size = IDs.shape
+    Y    = size[0]
+    X    = size[1]
+
+    indexInf     =  np.where(np.isinf(numpy_matrix))
+    indexDiscard =  np.where(np.isnan(numpy_matrix))
+    
+    numpy_matrix[indexInf] = np.nan # Replace Inf by NaN
+
+    strings     = np.array(["%s" % x for x in numpy_matrix])  #matrix has to be flattened for conversion to strings
+
+    for i in range(len(strings)):   #remove decimal place when floats are integers
+        string1 = strings[i]
+        string_split = string1.split('.')
+        if( len(string_split)==2):
+            if(string_split[1]=='0'):
+                integer = strings[i]
+                integer = integer[:-2]
+                strings[i] = integer
+    
+
+    strings[indexInf]     = 'N/A'    # use array of string for custumising hovering text
+    strings[indexDiscard] = 'N/A'
+
+    accession = accession.flatten()
+    accession[indexDiscard] = 'Discarded'
+    accession = accession.reshape(Y,X)                       
+
+    s_matrix = strings.reshape(Y,X)                  
+    s_matrix = np.flipud(s_matrix)      
+
+    numpy_matrix[indexInf] = np.inf # but back inf (N/A values)
+    numpy_matrix           = numpy_matrix.reshape(Y,X) 
+
+
+    numpy_matrix = np.flipud(numpy_matrix)  # For matching order of JS table
+    accession    = np.flipud(accession)        
+    plotID       = np.flipud(IDs)        
+
+    # Reverse Y ticks and start them from 1
+    Yvals = np.arange(0,Y)
+    Yaxis = np.arange(1,Y+1)
+    Yaxis = np.flip(Yaxis)
+    
+    Xvals = np.arange(0, X)
+    Xaxis = np.arange(1,X+1)
+
+    units = 'Units: '+unit
+
+    fig = px.imshow(numpy_matrix, aspect="auto",
+            labels=dict(x="columns", y="rows", color=units),
+            color_continuous_scale=px.colors.sequential.Greens, height=800 )
+    #print("_________",numpy_matrix[0][0], accession[0][0])
+    #print("_________", accession[0] )
+    #print(treatments)
+    if len(treatments)>0:
+        treatments = treatments.reshape(Y,X)
+        treatments = np.flipud(treatments)
+
+        fig.update_traces(
+        customdata = np.moveaxis([accession, s_matrix, plotID, treatments], 0,-1),
+        #hovertemplate="Accession: %{customdata[0]}<br>raw value: %{customdata[1]:.2f}  <extra></extra>")
+        hovertemplate="Accession: %{customdata[0]}<br>Raw value: %{customdata[1]}<br>Plot ID: %{customdata[2]} (column: %{x}, row: %{y})<br>Treatment: %{customdata[3]} <extra></extra>")
+
+    else:
+        fig.update_traces(
+        customdata = np.moveaxis([accession, s_matrix, plotID], 0,-1),
+        hovertemplate="Accession: %{customdata[0]}<br>Raw value: %{customdata[1]}<br>Plot ID: %{customdata[2]} (column: %{x}, row:%{y})<extra></extra>")
+        #check=np.moveaxis([accession, s_matrix, plotID, treatments], 0,-1) 
+        #print("PLOT_________", accession.shape )
+        fig.update_layout(font=dict(family="Courier New, monospace",size=12,color="Black"),title={
+        'text': title,
+        'y':0.98,'x':0.5,
+        'xanchor': 'center','yanchor': 'top'})
+
+    fig.update_layout( yaxis = dict(tickmode = 'array', tickvals = Yvals, ticktext = Yaxis ) )
+    fig.update_layout( xaxis = dict(tickmode = 'array', tickvals = Xvals, ticktext = Xaxis ) )
+
+
+    fig.update_xaxes(showgrid=True, gridwidth=7, gridcolor='Black', zeroline=False)
+    fig.update_yaxes(showgrid=True, gridwidth=7, gridcolor='Black', zeroline=False)
+    fig['layout'].update(plot_bgcolor='black')
+
+    #plot_div = plot([Scatter(x=x_data, y=y_data, mode='lines', name='test', opacity=0.8, marker_color='green')], output_type='div')
+    #plot_div = plotlyOffline(fig, output_type='div')
+    #fig.show()   ## ADDED ONLY FOR DASH TEST
+    
+    #return plot_div
+    return fig
 
 ####################################################################################
 def oddShapeValues(arraysJson, rows, columns, phenotype):
@@ -398,152 +565,4 @@ def oddShapePlotID(arraysJson, rows, columns, phenotype):
     matrix  = matrix.flatten()
 
     return matrix
-
-#####################################################################################################
-'''
-test rendering seaborn image
-'''
-#def seaborn_plot(numpy_matrix, title, unit, uuid, name):
-#def seaborn_plot(numpy_matrix, title, unit, name):
-def seaborn_plot(numpy_matrix, title, unit, name, color_map):
-
-    sns.set(rc={'figure.figsize':(15.5,6.5)})
-
-    numpy_matrix = np.flipud(numpy_matrix)      # To Match order shown originally in JS code
-    notAvailable = np.zeros_like(numpy_matrix)
-    discarded    =  np.zeros_like(numpy_matrix)
-    indexInf     =  np.where(np.isinf(numpy_matrix))
-    indexDiscard =  np.where(np.isnan(numpy_matrix))
-    notAvailable[indexInf]   = 1
-    discarded[indexDiscard]  = 1
-    NA        = np.where(notAvailable < 1, np.nan, notAvailable)
-    discarded = np.where(   discarded < 1, np.nan, discarded)
-    units = 'Units: '+ unit
-
-    numpy_matrix[indexInf] = np.nan # Replace Inf by NaN
-
-    # Reverse Y ticks and start them from 1
-    size  = numpy_matrix.shape
-    Y     = size[0]
-    Yvals = np.arange(0.5, Y+0.5, 1.0)
-    Yaxis = np.arange(1,Y+1)
-    Yaxis = np.flip(Yaxis)
-
-    X     = size[1]
-    Xvals = np.arange(0, X)
-    Xaxis = np.arange(1,X+1)
-
-
-    maxVal = np.nanmax(numpy_matrix)
-    minVal = np.nanmin(numpy_matrix)
-    #print(minVal)
-    colormap  = sns.light_palette(color_map, as_cmap=True)
-    dark      = sns.dark_palette((260, 75, 60), input="husl")
-    sns.heatmap(NA, linewidth=0.5,cmap=dark, cbar=False )
-
-    g = sns.heatmap(numpy_matrix,  vmax=maxVal, vmin=minVal,linewidth=0.5,cmap=colormap, cbar_kws={'label': units}) 
-    ##g.set_facecolor('xkcd:black')
-
-    g.patch.set_facecolor('white')
-    g.patch.set_edgecolor('black')
-    g.patch.set_hatch('xx')
-
-
-    g.set_xlabel("Columns", fontsize = 14)
-    g.set_ylabel("Rows", fontsize = 14)
-    g.set_title(title, fontsize = 20)
-    g.set_yticks( Yvals )
-    g.set_yticklabels(Yaxis, size=10)
-    g.tick_params(    axis='y', rotation=0)
-    g.set_xticks(Xvals)
-    g.set_xticklabels(Xaxis, size=10)
-   
-##############################################################################################
-'''
-test rendering plotly interactive heatmap
-'''
-#def plotly_plot(numpy_matrix, accession, title, unit, IDs, treatments):
-def plotly_plot(numpy_matrix, accession, title, unit, colormap):
-    #colormap = "Hot"
-    ##numpy_matrix = np.flipud(numpy_matrix)      # To Match order shown originally in JS code
-    #plotID      = np.flipud(IDs)        
-    size = accession.shape
-    Y    = size[0]
-    X    = size[1]
-
-    indexInf     =  np.where(np.isinf(numpy_matrix))
-    indexDiscard =  np.where(np.isnan(numpy_matrix))
-    
-    numpy_matrix[indexInf] = np.nan # Replace Inf by NaN
-
-    strings     = np.array(["%s" % x for x in numpy_matrix])  #matrix has to be flattened for conversion to strings
-
-    for i in range(len(strings)):   #remove decimal place when floats are integers
-        string1 = strings[i]
-        string_split = string1.split('.')
-        if( len(string_split)==2):
-            if(string_split[1]=='0'):
-                integer = strings[i]
-                integer = integer[:-2]
-                strings[i] = integer
-    
-
-    strings[indexInf]     = 'N/A'    # use array of string for custumising hovering text
-    strings[indexDiscard] = 'N/A'
-
-    accession = accession.flatten()
-    accession[indexDiscard] = 'Discarded'
-    accession = accession.reshape(Y,X)                       
-
-    s_matrix = strings.reshape(Y,X)                  
-    s_matrix = np.flipud(s_matrix)      
-
-    numpy_matrix[indexInf] = np.inf # but back inf (N/A values)
-    numpy_matrix           = numpy_matrix.reshape(Y,X) 
-
-
-    numpy_matrix = np.flipud(numpy_matrix)  # For matching order of JS table
-    accession    = np.flipud(accession)        
-    #plotID       = np.flipud(IDs)        
-
-    # Reverse Y ticks and start them from 1
-    Yvals = np.arange(0,Y)
-    Yaxis = np.arange(1,Y+1)
-    Yaxis = np.flip(Yaxis)
-    
-    Xvals = np.arange(0, X)
-    Xaxis = np.arange(1,X+1)
-
-    units = 'Units: '+unit
-    color = "px.colors.sequential."+colormap
-    #print(color)
-    #CM=eval("px.colors.sequential.Greens")
-    CM=eval(color)
-    #print(CM)
-    fig = px.imshow(numpy_matrix, aspect="auto",
-            labels=dict(x="columns", y="rows", color=units),
-            #color_continuous_scale=px.colors.sequential.Hot, height=800 )
-            color_continuous_scale=CM, height=800 )
-    #else:
-    fig.update_traces(
-    #customdata = np.moveaxis([accession, s_matrix, plotID], 0,-1),
-    customdata  = np.moveaxis([accession, s_matrix], 0,-1),
-    hovertemplate="Accession: %{customdata[0]}<br>Raw value: %{customdata[1]}<br> (column: %{x}, row:%{y})<extra></extra>")
-    fig.update_layout(font=dict(family="Courier New, monospace",size=12,color="Black"),title={
-    'text': title,
-    'y':0.98,'x':0.5,
-    'xanchor': 'center','yanchor': 'top'})
-
-    fig.update_layout( yaxis = dict(tickmode = 'array', tickvals = Yvals, ticktext = Yaxis ) )
-    fig.update_layout( xaxis = dict(tickmode = 'array', tickvals = Xvals, ticktext = Xaxis ) )
-
-
-    fig.update_xaxes(showgrid=True, gridwidth=7, gridcolor='Black', zeroline=False)
-    fig.update_yaxes(showgrid=True, gridwidth=7, gridcolor='Black', zeroline=False)
-    fig['layout'].update(plot_bgcolor='black')
-
-    #plot_div = plotlyOffline(fig, output_type='div')
-    fig.show()   ## ADDED ONLY FOR DASH TEST
-    
-    #return fig
 
